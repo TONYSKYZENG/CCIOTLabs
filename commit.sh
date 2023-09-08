a421ed4a02855272f0f6660c4727465cb807da7a
tony <292224750@qq.com>
2023-09-08 14:01:01 +0800

    1. first commit

---
+++ commit.sh
@@ -0,0 +1,9 @@
+
+BRANCH=lab1_2
+
+
+git init
+git checkout -b $BRANCH
+git add .
+git commit -F commit_info
+git push origin $BRANCH
---
+++ config.sh
@@ -0,0 +1,3 @@
+export IDF_PATH=~/Platforms/esp/v4_3/esp-idf
+source $IDF_PATH/export.sh
+idf.py menuconfig
